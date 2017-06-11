0e3ce5ef38bedba7c2223931937dd2b7a1542b7b
madanyli <madanyli@cisco.com>
2017-06-11 17:03:19 +0300

    Cleaned uped from project data

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,7 @@
 import os
 from init import tb, tb_mem  # init telebot and config obj
 from wrappers import secure, secure_call  # place for wrappers
-from markups import Main_insec, Main_sec, Projects
-from markups import Test, Environments, Actions
+from markups import Main_insec, Main_sec
 from markups import g_markup, g_inl_mark
 from actions import *  # place for actions
 
@@ -12,7 +11,6 @@ def markdown(text):
 
 
 @tb.message_handler(commands=['help', 'start', 'hi', 'hello'])
-@tb.message_handler(func=lambda m: m.text in ['Hi', 'H'])
 @secure
 def send_welcome(message):
     text = "Howdy, how are you doing?"
@@ -35,14 +33,6 @@ def send_welcome(message):
                         parse_mode="HTML")
 
 
-@tb.message_handler(commands=['test'])
-@secure
-def test_fun_list(message):
-    os.chdir(tb_mem.workDir)
-    tb.send_message(message.chat.id, "choose one of available tests:",
-                    reply_markup=Test)
-
-
 @tb.message_handler(commands=['sudo'])
 @secure
 def sudo(message):
@@ -55,44 +45,6 @@ def sudo(message):
         print(str(message.from_user.id) + " is trying sudo")
 
 
-@tb.message_handler(commands=['projects', 'p'])
-@secure
-def show_projects(message):
-    os.chdir(tb_mem.workDir)
-    tb.send_message(message.chat.id, 'Choose on of supported projects:',
-                    reply_markup=Projects)
-
-
-@tb.message_handler(commands=['Kafka', 'k'])
-@secure
-def Kafka(message):
-    path = tb_mem.workDir + "/aws-kafka-cluster/"
-    os.chdir(path)
-    ls(message)
-    tb.send_message(message.chat.id, "Which env?",
-                    reply_markup=Environments)
-
-
-@tb.message_handler(commands=['telebot', 't'])
-@secure
-def tbot(message):
-    path = tb_mem.workDir + "/tele-bot/"
-    os.chdir(path)
-    ls(message)
-    tb.send_message(message.chat.id, "What are you gonna do?",
-                    reply_markup=Actions)
-
-
-@tb.message_handler(commands=['dev', 'stage', 'prod'])
-@secure
-def chenv(message):
-    path = os.getcwd() + '/env-' + message.text.split()[0][1:]
-    os.chdir(path)
-    ls(message)
-    tb.send_message(message.chat.id, "What are you gonna do?",
-                    reply_markup=Actions)
-
-
 @tb.message_handler(commands=['make'])
 @secure
 def make_handle(message):
@@ -101,38 +53,16 @@ awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ \
 {split($1,A,/ /);for(i in A)print A[i]}'
 """
     # Execute command and get list of lines as output
-    L = (do_sh(command,message))
+    L = (do_sh(command, message))
     # Generate inline with needed prefix
     m = g_inl_mark(L, message.text.split()[0])
     tb.send_message(message.chat.id, '.' * 50, reply_markup=m)
 
-"""
-    if len(message.text.split()) == 1:
-        do(make_list, message)
-    else:
-        do("make " + message.text.split()[1], message)
-"""
 
 @tb.callback_query_handler(func=lambda call: '/make' in call.data)
 @secure_call
 def call_make(call):
     print(do_sh(call.data[1:]))
-
-
-@tb.message_handler(commands=['git'])
-@secure
-def git_handle(message):
-    status = "git branch -a |grep '*' ; git status"
-    if len(message.text.split()) == 1:
-        do(status, message)
-    else:
-        do("git " + message.text.split()[1], message)
-
-
-@tb.callback_query_handler(func=lambda call: True)
-@secure_call
-def call_back(call):
-    print(call.data)
 
 
 @tb.message_handler(commands=['ls'])
